e02e2e248aa194074ca7dff63de87a3a1ccd9970
Ángel Domínguez Ruiz <angeldominguez523@gmail.com>
2023-11-18 16:48:03 +0100

    hola

---
+++ HALLOWEEN/Halloween.js
@@ -0,0 +1,13 @@
+function show(element) {
+    document.getElementById(element).style.visibility="visible";
+}
+function hide(element) {
+    document.getElementById(element).style.visibility="hidden";
+}
+
+function sound(element){
+    document.getElementById(element).play();
+}
+function silence(element){
+    document.getElementById(element).pause();
+}
